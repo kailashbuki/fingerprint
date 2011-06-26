1b40c90a52f28c08facbf4341613d3c37b5c5833
Kailash Budhathoki <kailash.buki@gmail.com>
2011-06-26 12:31:38 +0545

    package, license file, version updated

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from distutils.core import setup
 
 setup(
     name='fingerprint',
-    version='1.0',
+    version='0.1.0',
     url='http://github.com/kailashbuki/fingerprint',
-    license='MIT',
+    license='LICENSE.txt',
     author='Kailash Budhathoki',
     author_email='kailash.buki@gmail.com',
     description='Document fingerprint generator'
                 'and ',
-    long_description=__doc__,
-    py_modules = ['fingerprintgenerator'],
+    long_description=open('README.txt').read(),
+    packages=['fingerprint'],
     zip_safe=False,
     platforms='any',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Application Development',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
