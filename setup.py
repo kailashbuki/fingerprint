70cd656d8164621f55482a7a710a3eb585635f8b
Kailash Budhathoki <kailash.buki@gmail.com>
2011-06-26 08:22:08 +0545

    setup file added

---
+++ setup.py
@@ -0,0 +1,23 @@
+setup(
+    name='fingerprint',
+    version='1.0',
+    url='http://github.com/kailashbuki/fingerprint',
+    license='MIT',
+    author='Kailash Budhathoki',
+    author_email='kailash.buki@gmail.com',
+    description='Document fingerprint generator'
+                'and ',
+    long_description=__doc__,
+    py_modules = ['fingerprintgenerator'],
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Application Development',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
